d3a9741551a36547e1ce6d1e0201d213a1f94181
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-05-31 09:52:03 +0200

    :sunny: regex + error messages corrected

--- js/script.js
+++ js/script.js
@@ -33,13 +33,22 @@ overlay.addEventListener('click', () => {
 // ________ REGEX ________
 btn.addEventListener('click', (e) => {
 
-    if(inputName.value.match(/^[a-zA-Z\ ]{3,50}$/i)) {
+    if(inputName.value.match(/^[a-zA-ZÜ-ü-' ]{3,25}$/i)) {
         goGreen(inputName);
         errorName.classList.add('dnone');
     } else {
         goRed(inputName);
         errorName.classList.remove('dnone');
         e.preventDefault();
+        if (inputName.value == "") {
+            errorName.innerHTML = "This field is required";
+        } else if (inputName.value.length < 3) {
+            errorName.innerHTML = "Your name should be at least 3 letters longer";
+        } else if(inputName.value.length > 25) {
+            errorName.innerHTML = "Should contain less than 25 symbols";
+        } else {
+            errorName.innerHTML = "Should only contain letters";
+        }
     }
 
     if(inputEmail.value.match(/^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/i)) {
@@ -49,15 +58,29 @@ btn.addEventListener('click', (e) => {
         goRed(inputEmail);
         errorEmail.classList.remove('dnone');
         e.preventDefault();
+        if(inputEmail.value == "") {
+            errorEmail.innerHTML = "This field is required";
+        } else {
+            errorEmail.innerHTML = "Should be of type \"example@email.com\"";
+        }
     }
 
-    if(inputMessage.value) {        // possible regex (inputMessage.value.match(/^.{6,930}$/i))
+    if(inputMessage.value.match(/^[a-zA-ZÜ-ü-'",!?;:()+=*/$€£&@. ]{10,200}$/i)) {
         goGreen(inputMessage);
         errorMessage.classList.add('dnone');
     } else {
         goRed(inputMessage);
         errorMessage.classList.remove('dnone');
         e.preventDefault();
+        if(inputMessage.value == "") {
+            errorMessage.innerHTML = "This field is required";
+        } else if(inputMessage.value.length < 10) {
+            errorMessage.innerHTML = "Give a more detailed description";
+        } else if(inputMessage.value.length > 200) {
+            errorMessage.innerHTML = "Should contain less than 200 symbols (you have put " + inputMessage.value.length + " symbols)";
+        } else {
+            errorMessage.innerHTML = "Avoid special symbols like # ~ µ _ etc.";
+        }
     }
 
     if(inputName.classList.contains('input-green') && inputEmail.classList.contains('input-green') && inputMessage.classList.contains('input-green')) {
