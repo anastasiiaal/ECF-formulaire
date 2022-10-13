1c409387f1b8a47df909bb8ec43411581d683f47
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-10-13 21:11:48 +0200

    :sparkles: ajax!

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
 // ________ elements from html ________
-const btn = document.getElementById("submit-btn"),
-      btnClose = document.getElementById("btn-close"),
-      overlay = document.getElementById("overlay");
+const submitForm = document.getElementById("form"),
+    btn = document.getElementById("submit-btn"),
+    btnClose = document.getElementById("btn-close"),
+    overlay = document.getElementById("overlay");
 
 const inputName = document.getElementById('name'),
-      inputEmail = document.getElementById('email'),
-      inputMessage = document.getElementById('message');
+    inputEmail = document.getElementById('email'),
+    inputMessage = document.getElementById('message');
 
 const errorName = document.getElementById('error-name'),
-      errorEmail = document.getElementById('error-email'),
-      errorMessage = document.getElementById('error-message');
+    errorEmail = document.getElementById('error-email'),
+    errorMessage = document.getElementById('error-message');
 
 // ________ functions to animate inputs ________
-function goGreen (i) {
+function goGreen(i) {
     i.classList.remove('input-red');
     i.classList.add('input-green');
 }
 
-function goRed (i) {
+function goRed(i) {
     i.classList.add('input-red');
 }
 
-// ________ event listeners ________
+function removeColor (a,b,c) {
+    a.classList.remove('input-green');
+    b.classList.remove('input-green');
+    c.classList.remove('input-green');
+}
+
+// ________ overlay event listeners ________
 btnClose.addEventListener('click', () => {
     overlay.classList.add("dnone");
 })
@@ -30,60 +37,87 @@ overlay.addEventListener('click', () => {
     overlay.classList.add("dnone");
 })
 
-// ________ REGEX ________
-btn.addEventListener('click', (e) => {
+// ________ btn event listener ________
+btn.addEventListener('click', () => {
+    if (inputName.value == "") {
+        goRed(inputName);
+        errorName.classList.remove('dnone');
+        errorName.innerHTML = "This field is required";
+    }
+    if (inputEmail.value == "") {
+        goRed(inputEmail);
+        errorEmail.classList.remove('dnone');
+        errorEmail.innerHTML = "This field is required";
+    }
+    if (inputMessage.value == "") {
+        goRed(inputMessage);
+        errorMessage.classList.remove('dnone');
+        errorMessage.innerHTML = "This field is required";
+    }
+})
 
-    if(inputName.value.match(/^[a-zA-ZÜ-ü-' ]{3,25}$/i)) {
+// ________ REGEX ________
+inputName.addEventListener("keyup", () => {
+    if (inputName.value.match(/^[a-zA-ZÜ-ü-' ]{3,25}$/i)) {
         goGreen(inputName);
         errorName.classList.add('dnone');
     } else {
         goRed(inputName);
         errorName.classList.remove('dnone');
-        e.preventDefault();
-        if (inputName.value == "") {
-            errorName.innerHTML = "This field is required";
-        } else if (inputName.value.length < 3) {
-            errorName.innerHTML = "Your name should be at least 3 letters longer";
-        } else if(inputName.value.length > 25) {
+
+        if (inputName.value.length < 3) {
+            errorName.innerHTML = "Your name should be at least 3 letters long";
+        } else if (inputName.value.length > 25) {
             errorName.innerHTML = "Should contain less than 25 symbols";
         } else {
             errorName.innerHTML = "Should only contain letters";
         }
     }
+});
 
-    if(inputEmail.value.match(/^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/i)) {
+inputEmail.addEventListener("keyup", () => {
+    if (inputEmail.value.match(/^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/i)) {
         goGreen(inputEmail);
         errorEmail.classList.add('dnone');
     } else {
         goRed(inputEmail);
         errorEmail.classList.remove('dnone');
-        e.preventDefault();
-        if(inputEmail.value == "") {
+        if (inputEmail.value == "") {
             errorEmail.innerHTML = "This field is required";
         } else {
             errorEmail.innerHTML = "Should be of type \"example@email.com\"";
         }
     }
+});
 
-    if(inputMessage.value.match(/^[a-zA-ZÜ-ü-'",!?;:()+=*/$€£&@. ]{10,200}$/i)) {
+inputMessage.addEventListener("keyup", () => {
+    if (inputMessage.value.match(/^[a-zA-ZÜ-ü0-9-'",!?;:()+=*/$€£&@. ]{10,200}$/i)) {
         goGreen(inputMessage);
         errorMessage.classList.add('dnone');
     } else {
         goRed(inputMessage);
         errorMessage.classList.remove('dnone');
-        e.preventDefault();
-        if(inputMessage.value == "") {
+        if (inputMessage.value == "") {
             errorMessage.innerHTML = "This field is required";
-        } else if(inputMessage.value.length < 10) {
+        } else if (inputMessage.value.length < 10) {
             errorMessage.innerHTML = "Give a more detailed description";
-        } else if(inputMessage.value.length > 200) {
+        } else if (inputMessage.value.length > 200) {
             errorMessage.innerHTML = "Should contain less than 200 symbols (you have put " + inputMessage.value.length + " symbols)";
         } else {
-            errorMessage.innerHTML = "Avoid special symbols like # ~ µ _ etc.";
+            errorMessage.innerHTML = "Avoid line breaking and special symbols like # ~ µ _ etc.";
         }
     }
-
-    if(inputName.classList.contains('input-green') && inputEmail.classList.contains('input-green') && inputMessage.classList.contains('input-green')) {
-        overlay.classList.remove('dnone');
-    }
 })
+
+// __________ MAIN FUNCTION SUBMIT __________ 
+submitForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let form = new FormData(submitForm);
+    fetch("./php/sendmail.php", {
+        method: "POST",
+        body: form,
+    });
+    document.dataForm.reset();
+    removeColor(inputName, inputEmail, inputMessage);
+    overlay.classList.remove('dnone');
+});
